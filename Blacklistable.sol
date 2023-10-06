8fbaca0a26349318c5702f89bb917711c10734d8
Elethcoin <147107814+Elethcoin@users.noreply.github.com>
2023-10-06 19:20:26 -0300

    Add files via upload

---
+++ Blacklistable.sol
@@ -0,0 +1,7 @@
+ragma solidity 0.8.16;
+
+interface Blacklistable {
+    function addRobotToBlacklist(address) external;
+    function removeRobotFromBlacklist(address) external;
+    function inRobotBlacklist(address) external view returns (bool);
+}
